dd724025a36907339e1b3425bbbb225627b5e117
Prakash Chand <Prakash0719pc@gmail.com>
2025-11-18 14:57:29 +0530

    feat: invite section complete

--- src/components/inviteSection.tsx
+++ src/components/inviteSection.tsx
@@ -2,7 +2,7 @@ import SvgIcon from "./SvgIcon";
 
 const InviteSection = () => {
   return (
-    <section className="w-full relative ">
+    <section className="w-full relative text-[#7A5192]">
       <img
         src="/assets/inviteSectionBg.png"
         alt="wedding-photo"
@@ -15,11 +15,50 @@ const InviteSection = () => {
       />
       <p
         className="
-        font-Jacques text-[26px] leading-[150%] font-normal absolute top-10 left-1/2 -translate-x-1/2 text-center text-[#BD8C1C]"
-        >
+        font-Jacques-plain text-[26px] leading-[150%] absolute top-10 left-1/2 -translate-x-1/2 text-center text-[#BD8C1C]"
+      >
         ॐ श्री गणेशाय नम
       </p>
-      <SvgIcon name="LordGanesh" className="w-41 h-53 absolute top-20 left-1/2 -translate-x-1/2" />
+      <SvgIcon
+        name="LordGanesh"
+        className="w-41 h-53 absolute top-20 left-1/2 -translate-x-1/2"
+      />
+      <p
+        className="
+        font-Jacques-plain  text-[42px] leading-[150%] absolute top-3/18 left-1/2 -translate-x-1/2 text-center "
+      >
+        With the heavenly blessings of <br />
+        Smt. Lata Devi & Sm. Kamal Kapoor <br /> and <br />
+        Mrs. Reena & Mr. Rajiv Kapoor
+      </p>
+      <p
+        className="
+        font-Jacques-plain text-8xl leading-[150%] absolute top-6/20 left-1/2 -translate-x-1/2 text-center text-[#BD8C1C]"
+      >
+        WE <br /> INVITE
+      </p>
+      <div
+        className="
+        font-Jacques-plain text-[42px] leading-[150%] absolute top-8/18 left-1/2 -translate-x-1/2 text-center "
+      >
+        You to join us in the wedding celebrations of <br />
+        <p className="mt-8">
+          Daughter of <br />
+        Mrs. Reena & Mr. Rajiv Kapoor  
+        </p>
+        
+      </div>
+      <p
+        className="font-Parisienne text-[42px] leading-[150%] text-center absolute top-11/18 left-2/8 -translate-x-1/2"
+      >
+        Abhishek <br /> & <br /> Anjali
+      </p>
+      <p
+        className="font-Jacques-plain text-[42px] leading-[150%] text-center absolute top-11/18 left-6/8 -translate-x-1/2"
+      >
+       Saturday, 21 June 2035 <br /> 123 Anywhere St., City, <br /> ST 12345
+      </p>
+
       <img
         src="/assets/boundaryImg.png"
         alt="wedding-photo"
@@ -28,7 +67,7 @@ const InviteSection = () => {
       <img
         src="/assets/coupleImg.png"
         alt="wedding-photo"
-        className="object-cover absolute bottom-0 left-1/2 -translate-x-1/2 "
+        className="object-cover absolute bottom-0 left-1/2 -translate-x-1/2 md:h-240 h-auto"
       />
     </section>
   );
