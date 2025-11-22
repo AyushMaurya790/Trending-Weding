262737abc5e9b96d492f6b60218204d1aac3682c
Prakash Chand <Prakash0719pc@gmail.com>
2025-11-22 11:00:45 +0530

    minor

--- src/app/template/[id]/page.tsx
+++ src/app/template/[id]/page.tsx
@@ -3,15 +3,17 @@ import EventShow from "@/components/EventShow";
 import ImageSection from "@/components/ImageSection";
 import InviteSection from "@/components/inviteSection";
 import SubDetails from "@/components/SubDetails";
+import EndDetails from "@/components/EndDetails";
 
 export default function TemplateDetail() {
   return (
-    <div className="min-h-screen bg-gray-50 overflow-hidden">
+    <div className="min-h-screen bg-[#f2e5d9] overflow-hidden">
       <TempHero />
       <InviteSection />
       <EventShow />
       <ImageSection />
       <SubDetails />
+      <EndDetails/>
     </div>
   );
 }
